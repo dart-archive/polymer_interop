6b69559a2caee0205c985a5bcae330bb99559fee
Kasper Peulen <kasperpeulen@gmail.com>
2015-06-23 13:02:05 +0200

    some work on PolymerDom class and a test

--- lib/polymer_interop.dart
+++ lib/polymer_interop.dart
@@ -20,98 +20,236 @@ class Polymer {
   static String camelToDashCase(String camel) =>
       (_CaseMap['camelToDashCase'] as JsFunction).apply([camel]);
 
+  /// Polymer provides a custom API for manipulating DOM such that local DOM and
+  /// light DOM trees are properly maintained. These methods and properties have
+  /// the same signatures as their standard DOM equivalents, except that properties
+  /// and methods that return a list of nodes return an Array, not a NodeList.
+  ///
+  /// Note: All DOM manipulation must use this API, as opposed to DOM API directly
+  /// on nodes.
+  ///
+  /// Using these node mutation APIs when manipulating children ensures that
+  /// shady DOM can distribute content elements dynamically. If you change
+  /// attributes or classes that could affect distribution without using the
+  /// Polymer.dom API, call distributeContent on the host element to force it
+  /// to update its distribution.
+  ///
+  /// Other resources: https://www.polymer-project.org/1.0/docs/devguide/local-dom.html
   static PolymerDom dom(node) => new PolymerDom(node);
 }
 
-/// Proxy for https://www.polymer-project.org/1.0/docs/devguide/local-dom.html.
+/// Polymer provides a custom API for manipulating DOM such that local DOM and
+/// light DOM trees are properly maintained. These methods and properties have
+/// the same signatures as their standard DOM equivalents, except that properties
+/// and methods that return a list of nodes return an Array, not a NodeList.
+///
+/// Note: All DOM manipulation must use this API, as opposed to DOM API directly
+/// on nodes.
+///
+/// Using these node mutation APIs when manipulating children ensures that
+/// shady DOM can distribute content elements dynamically. If you change
+/// attributes or classes that could affect distribution without using the
+/// Polymer.dom API, call distributeContent on the host element to force it
+/// to update its distribution.
+///
+/// Other resources: https://www.polymer-project.org/1.0/docs/devguide/local-dom.html
 class PolymerDom {
-  JsObject _proxy;
+  final JsObject _proxy;
 
+  final Node node;
+
+  /// The insert, append, and remove operations are transacted lazily in certain
+  /// cases for performance. In order to interrogate the dom (e.g. offsetHeight,
+  /// getComputedStyle, etc.) immediately after one of these operations, call
+  /// PolymerDom.flush() first.
   static void flush() => _Polymer['dom'].callMethod('flush');
 
-  PolymerDom(node) : _proxy = _Polymer.callMethod(
-      'dom', [node is Node ? new JsObject.fromBrowserObject(node) : node]);
+  PolymerDom(node)
+      : _proxy = _Polymer.callMethod('dom', [node]),
+        this.node = node;
 
+  /// Calling appendChild where the parent is a custom Polymer element adds the
+  /// node to the light DOM of the element.
+  ///
+  /// In order to append into the shadow root of a custom element, use
+  /// `this.root` as the parent.
+  ///
+  /// The appendChild method is transacted lazily in certain cases for
+  /// performance. In order to interrogate the dom (e.g. offsetHeight,
+  /// getComputedStyle, etc.) immediately after one of these operations, call
+  /// PolymerDom.flush() first.
+  Node append(node) => _proxy.callMethod('appendChild', [node]);
+
+  /// Read-only property that returns a live list of child nodes of the given element.
+  ///
+  /// The list also includes e.g. text nodes and comments. To skip them, use
+  /// `this.children` instead.
+  List<Node> get childNodes => _proxy['childNodes'];
+
+  /// Read-only property that returns a live `List` of the child elements of
+  /// `this`. The list doesn't include text nodes and comments.
   List<Element> get children => _proxy['children'];
 
-  List<Node> get childNodes => _proxy['childNodes'];
-  void set childNodes(List<Node> nodes) {
-    _proxy['childNodes'] = new JsObject.jsify(nodes);
-  }
+  // TODO: add docs
+  PolymerClassList get classList => new PolymerClassList(this);
 
-  Node get parentNode => _proxy['parentNode'];
-  void set parentNode(Node parent) {
-    _proxy['parentNode'] = new JsObject.fromBrowserObject(parent);
-  }
-
+  /// Read-only property that returns the node's first child in the tree, or
+  /// null if the node is childless.
   Node get firstChild => _proxy['firstChild'];
-  void set firstChild(Node child) {
-    _proxy['firstChild'] = new JsObject.fromBrowserObject(child);
-  }
 
-  Node get lastChild => _proxy['lastChild'];
-  void set lastChild(Node child) {
-    _proxy['lastChild'] = new JsObject.fromBrowserObject(child);
-  }
+  /// Read-only property that returns the object's first child Element, or null
+  /// if there are no child elements.
+  HtmlElement get firstElementChild => _proxy['firstElementChild'];
 
-  Element get firstElementChild => _proxy['firstElementChild'];
-  void set firstElementChild(Element child) {
-    _proxy['firstElementChild'] = new JsObject.fromBrowserObject(child);
-  }
-
-  Element get lastElementChild => _proxy['lastElementChild'];
-  void set lastElementChild(Element child) {
-    _proxy['lastElementChild'] = child;
-  }
-
-  Node get previousSibling => _proxy['previousSibling'];
-  void set previousSibling(Node sibling) {
-    _proxy['previousSibling'] = new JsObject.fromBrowserObject(sibling);
-  }
-
-  Node get nextSibling => _proxy['nextSibling'];
-  void set nextSibling(Node sibling) {
-    _proxy['nextSibling'] = new JsObject.fromBrowserObject(sibling);
-  }
-
-  String get text => _proxy['textContent'];
-  void set text(String text) { _proxy['textContent'] = text; }
-
-  String get innerHtml => _proxy['innerHTML'];
-  void set innerHtml(String html) {
-    _proxy['innerHTML'] = html;
-  }
-
-  List<String> get classes => _proxy['classList'];
-  void set classes(List<String> classes) {
-    _proxy['classList'] = new JsObject.jsify(classes);
-  }
-
-  Node querySelector(String selector) =>
-      _proxy.callMethod('querySelector', [selector]);
-
-  List<Node> querySelectorAll(String selector) =>
-      _proxy.callMethod('querySelectorAll', [selector]);
-
-  List<Node> getDistributedNodes() => _proxy.callMethod('getDistributedNodes');
-
+  /// Sometimes it’s necessary to access the elements which have been distributed
+  /// to a given <content> insertion point or to know to which <content> a given
+  /// node has been distributed. The getDistributedNodes and
+  /// getDestinationInsertionPoints methods, respectively, provide this information.
   List<Node> getDestinationInsertionPoints() =>
       _proxy.callMethod('getDestinationInsertionPoints');
 
-  void setAttribute(String attribute, String value) =>
-      _proxy.callMethod('setAttribute', [attribute, value]);
+  /// Sometimes it’s necessary to access the elements which have been distributed
+  /// to a given <content> insertion point or to know to which <content> a given
+  /// node has been distributed. The getDistributedNodes and
+  /// getDestinationInsertionPoints methods, respectively, provide this information.
+  List<Node> getDistributedNodes() => _proxy.callMethod('getDistributedNodes');
 
-  void removeAttribute(String attribute) =>
-      _proxy.callMethod('removeAttribute', [attribute]);
+  // TODO: find out what this does
+  getOwnerRoot() => _proxy.callMethod('getOwnerRoot');
 
-  Node append(Node node) =>
-      _proxy.callMethod('appendChild', [new JsObject.fromBrowserObject(node)]);
+  /// Sets or gets the HTML syntax describing the element's descendants.
+  String get innerHtml => _proxy['innerHTML'];
+  /// Sets or gets the HTML syntax describing the element's descendants.
+  void set innerHtml(String value) {
+    _proxy['innerHTML'] = value;
+  }
 
-  Node insertBefore(Node node, Node beforeNode) =>
-      _proxy.callMethod('appendChild', [
-        new JsObject.fromBrowserObject(node),
-        new JsObject.fromBrowserObject(beforeNode)]);
+  /// Calling `insertBefore` where parent is a custom Polymer element adds the
+  /// node to the light DOM of the element.
+  ///
+  /// In order to insert into the shadow root of a custom element, use
+  /// `this.root` as the parent.
+  ///
+  /// The insertBefore method is transacted lazily in certain cases for
+  /// performance. In order to interrogate the dom (e.g. offsetHeight,
+  /// getComputedStyle, etc.) immediately after one of these operations, call
+  /// PolymerDom.flush() first.
+  ///
+  /// [node] The node to insert
+  ///
+  /// [refNode] the reference node to insert [node] before
+  /// If [refNode] is null, this method wil act like `this.appendChild(node)`.
+  Node insertBefore(Node node, [Node refNode]) =>
+      _proxy.callMethod('insertBefore', [node, refNode]);
 
-  Node removeChild(Node node) =>
-      _proxy.callMethod('removeChild', [new JsObject.fromBrowserObject(node)]);
+  /// Read-only property that returns the last child of the node. If its parent
+  /// is an element, then the child is generally an element node, a text node,
+  /// or a comment node. It returns null if there are no child elements.
+  Node get lastChild => _proxy['lastChild'];
+
+  /// Read-only property  that returns the object's last child `Element` or null
+  /// if there are no child elements.
+  HtmlElement get lastElementChild => _proxy['lastElementChild'];
+
+  /// Read-only property that returns the node immediately following the specified
+  /// one in its parent's childNodes list, or null if the specified node is the
+  /// last node in that list.
+  Node get nextSibling => _proxy['nextSibling'];
+
+  /// Read-only property returns the element immediately following the specified
+  /// one in its parent's children list, or null if the specified element is the
+  /// last one in the list.
+  HtmlElement get nextElementSibling => _proxy['nextElementSibling'];
+
+  /// Read-only property that returns the parent of the specified node in the
+  /// DOM tree.
+  Node get parentNode => _proxy['parentNode'];
+
+  /// Read-only property returns the Element immediately prior to the specified
+  /// one in its parent's children list, or null if the specified element is the
+  /// first one in the list.
+  HtmlElement get previousElementSibling => _proxy['previousElementSibling'];
+
+  /// Read-only property that returns the node immediately preceding the specified
+  /// one in its parent's childNodes list, or null if the specified node is the
+  /// first in that list.
+  Node get previousSibling => _proxy['previousSibling'];
+
+  /// Returns a list of nodes distributed within _proxy.callMethod element. These can be
+  /// dom children or elements distributed to children that are insertion
+  /// points.
+  List<HtmlElement> queryDistributedElements(String selectors) =>
+      _proxy.callMethod('queryDistributedElements', [selectors]);
+
+  /// Returns the first element within the document that matches the specified
+  /// group of selectors.
+  HtmlElement querySelector(String selectors) =>
+      _proxy.callMethod('querySelector', [selectors]);
+
+  /// Returns a non-live NodeList of all elements descended from the element on
+  /// which it is invoked that match the specified group of CSS selectors.
+  List<HtmlElement> querySelectorAll(String selectors) =>
+      _proxy.callMethod('querySelectorAll', [selectors]);
+
+  /// Removes an attribute from the the element.
+  void removeAttribute(String name) =>
+      _proxy.callMethod('removeAttribute', [name]);
+
+  /// Removes the given `node` from the element's `lightChildren`.
+  /// _proxy.callMethod method also performs dom composition.
+  ///
+  /// The removeChild method is transacted lazily in certain cases for
+  /// performance. In order to interrogate the dom (e.g. offsetHeight,
+  /// getComputedStyle, etc.) immediately after one of these operations, call
+  /// PolymerDom.flush() first.
+  Node removeChild(Node node) => _proxy.callMethod('removeChild', [node]);
+
+  /// Replaces one child node of the specified element with another.
+  Node replaceChild(Node newChild, Node oldChild) =>
+      _proxy.callMethod('replaceChild', [newChild, oldChild]);
+
+  /// Adds a new attribute or changes the value of an existing attribute on the
+  /// element.
+  void setAttribute(String name, String value) =>
+      _proxy.callMethod('setAttribute', [name, value]);
+
+  /// Represents the text content of a node and its descendants.
+  String get text => _proxy['textContent'];
+
+  /// Sets the text content of a node and its descendants.
+  void set text(String text) {
+    _proxy['textContent'] = text;
+  }
+}
+
+// TODO: add docs
+class PolymerClassList {
+  final PolymerDom domApi;
+
+  final HtmlElement node;
+
+  final JsObject _proxy;
+
+  PolymerClassList(PolymerDom host)
+      : domApi = host,
+        node = host.node,
+        _proxy = host._proxy["classList"];
+
+  /// Adds a class to an element's list of classes. If [value] already exists in
+  /// the element's list of classes, it will not add the class again.
+  add(String value) => _proxy.callMethod('add', [value]);
+
+  /// Adds a list of classes to an element's list of classes. If a class already
+  /// exists in the element's list of classes, it will not add the class again.
+  addAll(Iterable<String> values) => _proxy.callMethod('add', values);
+
+  /// Removes a class from an element's list of classes. If class does not
+  /// exist in the element's list of classes, it will not throw an error or
+  /// exception.
+  remove(String value) => _proxy.callMethod('remove', [value]);
+
+  /// Toggles the existence of a class in an element's list of classes. If
+  /// [shouldAdd] is true, _proxy.callMethod method will act the same as `_proxy.callMethod.add`.
+  toggle(String value, [bool shouldAdd]) =>
+      _proxy.callMethod('toggle', [value, shouldAdd]);
 }
---
+++ tool/grind.dart
@@ -0,0 +1,24 @@
+library polymerjs.grind;
+
+import 'package:grinder/grinder.dart';
+
+const dartium = 'dartium';
+const chrome = 'chrome';
+const firefox = 'firefox';
+const safari = 'safari';
+
+const macPlatforms = const ['dartium,firefox,chrome,safari'];
+
+const PolymerDom = 'test/PolymerDom_test.dart';
+
+const allFiles = const [
+  PolymerDom
+];
+
+main(args) => grind(args);
+
+@DefaultTask('Test')
+test() =>
+    new TestRunner().test(
+        files: allFiles,
+        platformSelector: macPlatforms);
