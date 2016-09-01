41123fcd18a92e498dabdf5808a3592d05a93772
Vittorio Ballestra <vittorio.ballestra@drafintech.it>
2016-09-01 09:59:31 +0200

    adding conversion strategies support

---
+++ lib/polymer_interop_config.dart
@@ -0,0 +1,27 @@
+library polymer_interop.polymer_interop_config;
+
+///
+/// Configuration options for polymer interop features.
+///
+
+///
+/// Possible strategies that can be applied in `convertToJs` / `convertToDart`
+///
+enum JsConversionStrategy {
+  /// Create a deep js copy of the dart object.
+  deepCopy,
+
+  /// Uses ES6 proxies.
+  /// *warning* experimental feature that may cause browser compatibility issues
+  es6Proxy
+}
+
+class PolymerInteropConfiguration {
+  /// Conversion strategy to be applied to `List` derived objects
+  static JsConversionStrategy listConversionStrategy =
+      JsConversionStrategy.deepCopy;
+
+  /// Conversion strategy to be applied to `Map` derived objects
+  static JsConversionStrategy mapConversionStrategy =
+      JsConversionStrategy.deepCopy;
+}
---
+++ lib/src/convert_es6_proxy.dart
@@ -0,0 +1,59 @@
+@HtmlImport('convert_es6_proxy.html')
+library polymer_interop.lib.src.convert_es_proxy;
+
+import 'package:web_components/web_components.dart' show HtmlImport;
+
+import 'dart:js';
+
+import 'package:polymer_interop/polymer_interop.dart';
+export 'package:polymer_interop/polymer_interop.dart'
+    show convertToDart, convertToJs;
+
+final JsObject _polymerInteropDartES6 = context['Polymer']['PolymerInterop']['ES6'];
+
+final JsFunction createES6JsProxyForArray = _initES6ListProxySupport();
+
+final JsObject _Unsupported = _polymerInteropDartES6['Unsupported'];
+
+/// Hooks for ES6 Proxies
+///
+/// Hooks for getting and setting properties and methods from and to dart objects.
+///
+JsFunction _initES6ListProxySupport() {
+  <String, Function>{
+    '_dartArrayGet': (List instance, index) =>
+        (index is num) ? convertToJs(instance[index]) : _Unsupported,
+    '_dartArrayPut': (List instance, int index, val) {
+      if (index is num) {
+        instance[index] = convertToDart(val);
+      } else {
+        return _Unsupported;
+      }
+    },
+    '_dartArrayLength': (List instance) => instance.length,
+    '_dartArraySplice': (List instance, int index, int howmany, List items) {
+      index = index == null ? 0 : index;
+      int end = (howmany == null) ? instance.length : index + howmany;
+
+      JsArray removed =
+          new JsArray.from(instance.sublist(index, end).map(convertToJs));
+
+      items = items == null ? [] : items;
+
+      instance.replaceRange(index, end, items.map(convertToDart));
+
+      return removed;
+    },
+    '_dartArraySlice': (List instance, [int begin, int end]) {
+      begin = begin == null ? 0 : begin;
+      return new JsArray.from(instance.sublist(begin, end).map(convertToJs));
+    },
+    '_dartArrayPush': (List instance, List items) {
+      instance.addAll(items.map(convertToDart));
+    }
+  }.forEach((String k, Function fun) {
+    _polymerInteropDartES6[k] = fun;
+  });
+
+  return   _polymerInteropDartES6['createES6JsProxyForArray'];
+}
